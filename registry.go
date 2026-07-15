2b542139804844c856a1e160aec8bdcd9f9df80b
xogas <57179186+xogas@users.noreply.github.com>
2026-07-15 21:03:44 +0800

    refactor: drop registry.go and relocate Binder next to Proxy

--- registry.go
+++
@@ -1,8 +0,0 @@
-package wayland
-
-// Binder is implemented by objects that can bind a Wayland protocol interface
-// to a server-side global. The generated BindXxx functions accept a Binder
-// so they can be used with any client-side proxy that supports binding.
-type Binder interface {
-	Bind(name uint32, iface string, version uint32) (*Proxy, error)
-}
